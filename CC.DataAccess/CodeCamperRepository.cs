938dd288b76b044f3bdf83cc1fc2eac85fb7472b
Ahsan <vampxlr@yahoo.com>
2014-04-27 23:49:53 +0600

    Single Page First Commit

---
+++ CC.DataAccess/CodeCamperRepository.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+using Breeze.WebApi;
+using Newtonsoft.Json.Linq;
+using CC.Model;
+
+namespace CC.DataAccess
+{
+    /// <summary>
+    /// Repository (a "Unit of Work" really) of CodeCamper models.
+    /// </summary>
+    public class CodeCamperRepository
+    {
+        private readonly Breeze.WebApi.EF.EFContextProvider<CodeCamperDbContext>
+            _contextProvider = new Breeze.WebApi.EF.EFContextProvider<CodeCamperDbContext>();
+
+        private CodeCamperDbContext Context { get { return _contextProvider.Context; } }
+
+        public string Metadata
+        {
+            get { return _contextProvider.Metadata(); }
+        }
+
+        public SaveResult SaveChanges(JObject saveBundle)
+        {
+            return _contextProvider.SaveChanges(saveBundle);
+        }
+
+        public IQueryable<Session> Sessions
+        {
+            get { return Context.Sessions; }
+        }
+
+        public IQueryable<Person> Speakers
+        {
+            get { return Context.Persons.Where(p => p.SpeakerSessions.Any()); }
+        }
+
+        public IQueryable<Person> Persons
+        {
+            get { return Context.Persons; }
+        }
+
+        public IQueryable<Room> Rooms
+        {
+            get { return Context.Rooms; }
+        }
+        public IQueryable<TimeSlot> TimeSlots
+        {
+            get { return Context.TimeSlots; }
+        }
+        public IQueryable<Track> Tracks
+        {
+            get { return Context.Tracks; }
+        }
+    }
+}
---
+++ SPA/app/ireen/ireen.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'ireen';
+    angular.module('app').controller(controllerId, ['common', 'datacontext', ireen]);
+
+    function ireen(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Ireen';
+        vm.books = [];
+        activate();
+    
+        function activate() {
+            var promises = [ getBooks()];
+            common.activateController(promises, controllerId)
+                .then(function () { log('Activated Ireen View'); });
+        }
+
+        function getBooks() {
+            return datacontext.getBooks().then(function (data) {
+                return vm.books = data;
+            });
+        }
+
+    }
+})();
