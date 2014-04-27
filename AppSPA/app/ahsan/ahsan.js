64ad3bc6447bebcef3142c6f8dd02a789ae85bc3
Ahsan <vampxlr@yahoo.com>
2014-04-28 00:09:51 +0600

    Single Page First Commit 3rd commit

---
+++ AppSPA/app/ahsan/ahsan.js
@@ -0,0 +1,20 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'ahsan';
+    angular.module('app').controller(controllerId, ['common', ahsan]);
+
+    function ahsan(common) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        vm.title = 'Ahsan';
+
+        activate();
+
+        function activate() {
+            common.activateController([], controllerId)
+                .then(function () { log('Activated Ahsan View'); });
+        }
+    }
+})();
